2ae343cf493618902f84846ee41408d4a03d57d4
Truong Le Vinh Phuc <truonglevinhphuc2006@gmail.com>
2024-10-25 15:38:41 +0700

    Refactor RefreshToken model and repository
    - Add _id property to RefreshToken model
    - Generate a new ObjectId for _id in CreateRefreshTokenAsync method
    - Update GetRefreshTokenByTokenAsync method to use FindFirstOrDefaultAsync
    - Update RevokeRefreshTokenAsync method to use UpdateOneAsync

--- OnlineContestManagement/Data/Models/RefreshToken.cs
+++ OnlineContestManagement/Data/Models/RefreshToken.cs
@@ -1,9 +1,15 @@
 using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace OnlineContestManagement.Data.Models
 {
     public class RefreshToken
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string _id { get; set; }
+
         [Key]
         public string Token { get; set; }
         public string UserId { get; set; }
